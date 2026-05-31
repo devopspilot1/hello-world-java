4785fe721e917af1aeed99fbc103303908a7947f
Vignesh Sweekaran <vigneshsweekaran@gmail.com>
2026-05-31 13:52:09 +0800

    refactor: migrate servlet dependencies from javax to jakarta namespace

--- src/main/java/com/helloworld/HelloWorld.java
+++ src/main/java/com/helloworld/HelloWorld.java
@@ -1,14 +1,12 @@
 package com.helloworld;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import jakarta.servlet.http.*;
 import java.io.*;
 
-public class HelloWorld extends HttpServlet 
-{
-  public void service( HttpServletRequest req, HttpServletResponse res ) throws IOException {
+public class HelloWorld extends HttpServlet {
+  public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
     PrintWriter out = res.getWriter();
-    out.println( "Hello, World!" );
+    out.println("Hello, World!");
     out.close();
   }
 }
